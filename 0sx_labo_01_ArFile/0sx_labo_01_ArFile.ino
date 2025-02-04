f9e27fa4bbac103345aec50256d90fcdf84f139c
Maskaroni <MiEveryone@outlook.com>
2025-02-04 09:45:43 -0500

    Version finale du tp1.

--- 0sx_labo_01_ArFile/0sx_labo_01_ArFile.ino
+++ 0sx_labo_01_ArFile/0sx_labo_01_ArFile.ino
@@ -1,9 +1,53 @@
-void setup() {
-  // put your setup code here, to run once:
+const int LED = 13;
+const int OFF = 0;
+const int ON = 255;
 
+void setup() {
+  pinMode(LED, OUTPUT);
+  Serial.begin(9600);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  allume();
+  clignote();
+  variation();
+  stop();
+}
 
+
+void allume() {
+  Serial.println("Allumé - 2486739");
+  analogWrite(LED, ON);
+  delay(2000);
+}
+
+void clignote() {
+  Serial.println("Clignotement - 2486739");
+  for (int i = 1; i <= 3; i++) {
+    analogWrite(LED, ON);
+    delay(250);
+    analogWrite(LED, OFF);
+    delay(250);
+  }
+}
+
+void variation() {
+  const int TIME_TAKEN = 2048;
+  const int JUMPS = 15;
+  const int FADE = (ON + 1) / JUMPS;
+  const int DELAY_VARIATION = TIME_TAKEN / JUMPS;
+  int fading_variable = 255;
+
+  Serial.println("Variation - 2486739");
+
+  for (fading_variable = 255; fading_variable >= OFF; fading_variable -= FADE) {
+    analogWrite(LED, fading_variable);
+    delay(DELAY_VARIATION);
+  }
+}
+
+void stop() {
+  Serial.println("Éteint - 2486739");
+  analogWrite(LED, OFF);
+  delay(2000);
 }
